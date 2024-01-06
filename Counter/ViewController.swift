3d1c82349eb8466f2edbd0a720ad3b8d8d3bd9e2
Dmitriy Chicherin <dmitriy.chicherin@yandex.ru>
2024-01-06 20:26:40 +0300

    Выполнены условия базовой задачи.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter: Int = 0    // Переменная счетчика, иницилизируем 0
+    
+    func counterLabelRefresh() {
+        if let label = self.view.viewWithTag(2) as? UILabel {   // Получаем доступ к Label
+            label.text = "Значение счетчика:\n\(counter)"       // Обновляем значением counter
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabelRefresh()   // При загрузке формы, обновляем Label
     }
 
-
+    @IBAction func countIncButton(_ sender: Any) {
+        counter += 1            // Увеличиваем на 1 значение counter
+        counterLabelRefresh()   // Вызываем функцию обновления Label
+    }
+    
 }
 
