630d6bad2caf7c15b7db1a3400e4cf627da9cd3f
Dmitriy Chicherin <dmitriy.chicherin@yandex.ru>
2024-01-10 10:40:35 +0300

    Правки после ревью 1. Изменена область видимости для фукнций и переменных 2. Поправлены комментарии и удалены лишние 3. Поправлены отступы и структура

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,68 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var counter: Int = 0            // Переменная счетчика, иницилизируем 0
-    var dateF = DateFormatter()     // Переменная для формата даты и времени
+    private var counter: Int = 0                    // Счётчик
+    private var dateFormatter = DateFormatter()     // Формат даты и времени
 
-    
-    func CounterLabelRefresh() {    // Процедура обновления Label
-        if let label = self.view.viewWithTag(2) as? UILabel {   // Получаем доступ к Label
-            label.text = "Значение счетчика:\n\(counter)"       // Обновляем значением counter
-        }
-
-    }
-    
-    func TextViewUpdate (with status:String) {
-        if let textView = self.view.viewWithTag(5) as? UITextView {
-            let dateNow = dateF.string(from: Date())
-            switch status {
-            case "Init":
-                textView.text = "История изменений:"
-                dateF.dateFormat = "yy-MM-dd HH:mm:ss"      // Определение формата даты и времени для лога
-            case "Reset":
-                textView.text += "\n[\(dateNow)]: значение сброшено"                            //Делаем запись в лог о сбросе
-            case "Increase":
-                textView.text += "\n[\(dateNow)]: значение изменено на +1"                      //Делаем запись в лог об увеличении значения
-            case "Decrease":
-                textView.text += "\n[\(dateNow)]: значение изменено на -1"                      //Делаем запись в лог об уменьшении значения
-            case "BelowZero":
-                textView.text += "\n[\(dateNow)]: попытка уменьшить значение счётчика ниже 0"   //Делаем запись в лог о попытке уменьшения ниже границы
-            default:
-                print("n/a")
-            }
-            textView.scrollRectToVisible(textView.caretRect(for: textView.endOfDocument), animated: true) // Скролл на новую строчку
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        CounterLabelRefresh()                       // При загрузке формы, обновляем Label - устанавливаем 0
-        TextViewUpdate(with: "Init")                // При загрузке формы пишем в TextView базовую строчку и устанавливаем формат даты и времени
+        CounterLabelRefresh()                       // Устанавливаем счетчик - 0
+        TextViewUpdate(with: "Init")                // Пишем в TextView начальную строку и устанавливаем формат даты и времени
     }
-
+    
     @IBAction func countIncButton(_ sender: Any) {
-        counter += 1            // Увеличиваем на 1 значение counter
-        CounterLabelRefresh()   // Вызываем функцию обновления Label
-        TextViewUpdate(with: "Increase") //Вызываем функцию добавления строки TextView с +
+        counter += 1                                // Увеличиваем на 1 значение counter
+        CounterLabelRefresh()                       // Вызываем функцию обновления Label
+        TextViewUpdate(with: "Increase")            // Вызываем функцию добавления строки TextView с +
     }
     
     @IBAction func countDecButton(_ sender: Any) {
-        if counter > 0 {        // Проверяем, что counter больше 0, чтобы уменьшать
-            counter -= 1        // Уменьшваем на 1 значение counter
-            CounterLabelRefresh()   // Вызываем функцию обновления Label
-            TextViewUpdate(with: "Decrease") //Вызываем функцию добавления строки TextView с -
+        if counter > 0 {                            // Проверяем, что counter больше 0, чтобы уменьшать
+            counter -= 1                            // Уменьшаем на 1 значение counter
+            CounterLabelRefresh()                   // Вызываем функцию обновления Label
+            TextViewUpdate(with: "Decrease")        // Вызываем функцию добавления строки TextView с -
         } else {
-            TextViewUpdate(with: "BelowZero")//Вызываем функцию добавления строки TextView с <0
+            TextViewUpdate(with: "BelowZero")       //Вызываем функцию добавления строки TextView с <0
         }
     }
     
     @IBAction func countResButton(_ sender: Any) {
-        counter = 0             // Обнуляем counter
-        CounterLabelRefresh()   // Вызываем функцию обновления Label
-        TextViewUpdate(with: "Reset") //Вызываем функцию добавления строки TextView с 0
+        counter = 0                                 // Обнуляем counter
+        CounterLabelRefresh()                       // Вызываем функцию обновления Label
+        TextViewUpdate(with: "Reset")               // Вызываем функцию добавления строки TextView с 0
+    }
+
+    private func CounterLabelRefresh() {            // Функция обновления счётчика
+        if let label = self.view.viewWithTag(2) as? UILabel {
+            label.text = "Значение счетчика:\n\(counter)"
+        }
+        
     }
     
-    
+    private func TextViewUpdate (with status:String) {          // Функция обновления TextViewUpdate
+        if let TextView = self.view.viewWithTag(5) as? UITextView {
+            let dateNow = dateFormatter.string(from: Date())
+            switch status {
+            case "Init":
+                TextView.text = "История изменений:"
+                dateFormatter.dateFormat = "yy-MM-dd HH:mm:ss"  // Установка формата даты и времени
+            case "Reset":
+                TextView.text += "\n[\(dateNow)]: значение сброшено"
+            case "Increase":
+                TextView.text += "\n[\(dateNow)]: значение изменено на +1"
+            case "Decrease":
+                TextView.text += "\n[\(dateNow)]: значение изменено на -1"
+            case "BelowZero":
+                TextView.text += "\n[\(dateNow)]: попытка уменьшить значение счётчика ниже 0"
+            default:
+                print("n/a")
+            }
+            TextView.scrollRectToVisible(TextView.caretRect(for: TextView.endOfDocument), animated: true) //Скролл на нижнюю строку
+        }
+    }
 }
 
